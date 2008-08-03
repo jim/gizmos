e2d929ed183120dbc9c8722ba3f88f530b056c91
Jim Benton <jim@autonomousmachine.com>
2008-08-03 12:14:11 -0500

    Fixed a bug in cycle having to do with array slicing

--- cycle/cycle.js
+++ cycle/cycle.js
@@ -1,32 +1,31 @@
 var Cycle = function(){
-    var cycles = {};
+    var cycles, that, thru, reset;
     
-    var that = {};
-    
-    var thru = function(){
+    cycles = {};
+    that = {};
+
+    thru = function(){
         var values, i, cycle, options, name, nextValue;
-        
+
         values = Array.prototype.slice.call(arguments);
-        options = typeof(values.slice(-1)) === 'object' ? values.pop() : {};
-        
+        options = typeof(values.slice(-1)[0]) === 'object' ? values.pop() : {};
+
         name = options['name'] || 'default';
-        
-        if(cycles[name]) {
-            cycle = cycles[name];
-            nextValue = cycle.values[cycle.index];
-            cycle.index = (cycle.index === cycle.values.length -1) ? 0 : (cycle.index +1);
-            return nextValue;
-        } else {
+
+        if(!cycles[name]) {
             cycles[name] = {
               values: values,
               index: 0
-            };
-            return thru.apply(that, values);
-        }
+            }; 
+        };
+        cycle = cycles[name];
+        nextValue = cycle.values[cycle.index];
+        cycle.index = (cycle.index === cycle.values.length -1) ? 0 : (cycle.index +1);
+        return nextValue;
     };
     that.thru = thru;
-    
-    var reset = function(name) {
+
+    reset = function(name) {
         if(cycles[name]) {
             cycles[name].index = 0;
         }
@@ -34,5 +33,5 @@ var Cycle = function(){
     that.reset = reset;
     
     return that;
-    
+
 }();
